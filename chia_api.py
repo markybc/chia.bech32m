547da4eff860c31baaf76c9b88ae665241d0ffa9
mengyaoyu <915019395@qq.com>
2021-05-17 14:01:20 +0800

    Update chia_api.py

--- chia_api.py
+++ chia_api.py
@@ -1,16 +1,20 @@
 from bech32m import decode_puzzle_hash, encode_puzzle_hash
-from bottle import route, run
+from bottle import route, run, template
 from byte_types import hexstr_to_bytes
 
 
-@route('/decode/<address>')
-def decode(address):
-    return decode_puzzle_hash(address).hex()
+@route('/getPuzzleHash/<address>')
+def getPuzzleHash(address):
+    puzzleHash = decode_puzzle_hash(address).hex()
+    res = {"code": "200", "data": puzzleHash}
+    return res
 
 
-@route('/encode/<puzzleHash>')
-def encode(puzzleHash):
-    return encode_puzzle_hash(hexstr_to_bytes(puzzleHash), "xch")
+@route('/getAddress/<puzzleHash>')
+def getAddress(puzzleHash):
+    address = encode_puzzle_hash(hexstr_to_bytes(puzzleHash), "xch")
+    res = {"code": "200", "data": address}
+    return res
 
 
 run(host='localhost', port=9988)
